b5ba0c40628b6071fa26e8f14423742dbe349993
FausticSun <fausticsun@gmail.com>
2017-03-13 18:27:58 +0800

    Modfied invalid args test case Previous test case was valid (Everything after add will be part the task name). Changed it to not supplying any arguments at all).

--- src/test/java/werkbook/task/logic/LogicManagerTest.java
+++ src/test/java/werkbook/task/logic/LogicManagerTest.java
@@ -200,7 +200,8 @@ public class LogicManagerTest {
     @Test
     public void execute_add_invalidArgsFormat() {
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE);
-        assertCommandFailure("add wrong args wrong args", expectedMessage);
+        // Adding without name or arguments
+        assertCommandFailure("add", expectedMessage);
     }
 
     @Test
