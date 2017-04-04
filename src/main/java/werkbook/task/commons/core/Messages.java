e31e9522e42175010b3e83015bed4690759cbfbb
pzyu <pangzhengyu@live.com>
2017-04-04 22:25:32 +0800

    Add system message for empty command Change messages to sound more personal

--- src/main/java/werkbook/task/commons/core/Messages.java
+++ src/main/java/werkbook/task/commons/core/Messages.java
@@ -5,9 +5,11 @@ package werkbook.task.commons.core;
  */
 public class Messages {
 
-    public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
-    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "The task index provided is invalid";
+    public static final String MESSAGE_EMPTY_COMMAND = "Did you mean to type something?";
+    public static final String MESSAGE_UNKNOWN_COMMAND = "I'm sorry, I don't recognize that command."
+            + "\nTry typing in 'help' to see the list of commands you can use!";
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "You've got the wrong command format! \n%1$s";
+    public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "I can't seem to find this task :(";
     public static final String MESSAGE_TASKS_LISTED_OVERVIEW = "%1$d tasks listed!";
 
 }
