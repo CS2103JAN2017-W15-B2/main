7238f420abc33fb61bbea90638d9d59828bdf140
FausticSun <FausticSun@gmail.com>
2017-04-04 22:13:30 +0800

    Implemented GImport Command 2 Forgot to stage file

---
+++ src/main/java/werkbook/task/logic/commands/GImportCommand.java
@@ -0,0 +1,32 @@
+//@@author A0162266E
+package werkbook.task.logic.commands;
+
+import werkbook.task.logic.commands.exceptions.CommandException;
+import werkbook.task.model.task.UniqueTaskList;
+
+/**
+ * Terminates the program.
+ */
+public class GImportCommand extends Command {
+
+    public static final String COMMAND_WORD = "gimport";
+
+    public static final String GOOGLE_IMPORTED_ACKNOWLEDGEMENT = "Imported Tasks from Google Tasks";
+
+    @Override
+    public CommandResult execute() throws CommandException {
+        try {
+            UniqueTaskList gTaskList = gtasks.retrieve();
+            model.sync(gTaskList);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new CommandException(e.getMessage());
+        }
+        return new CommandResult(GOOGLE_IMPORTED_ACKNOWLEDGEMENT);
+    }
+
+    @Override
+    public boolean isMutable() {
+        return true;
+    }
+}
