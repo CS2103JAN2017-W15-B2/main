8f0c2d7c83242bf466358f027ed15d83152988d1
invisidark <vpsy28@gmail.com>
2017-04-07 02:05:50 +0800

    modified sample data

--- src/main/java/werkbook/task/model/util/SampleDataUtil.java
+++ src/main/java/werkbook/task/model/util/SampleDataUtil.java
@@ -10,30 +10,20 @@ import werkbook.task.model.task.Name;
 import werkbook.task.model.task.StartDateTime;
 import werkbook.task.model.task.Task;
 import werkbook.task.model.task.UniqueTaskList;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class SampleDataUtil {
     public static Task[] getSampleTasks() {
+    	Calendar cal = Calendar.getInstance();
+    	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HHmm");
         try {
             return new Task[] {
-                new Task(new Name("Walk the dog"),
-                        new Description("Take Zelda on a walk around the park"),
-                        new StartDateTime("03/01/2016 0900"), new EndDateTime("03/01/2016 1100"),
+                new Task(new Name("Welcome to werkbook! Start by adding a task."),
+                        new Description("Try: add <name of task> (description)"),
+                        new StartDateTime("03/01/2016 0900"), new EndDateTime(sdf.format(cal.getTime())),
                         new UniqueTagList("Incomplete")),
-                new Task(new Name("Get groceries"), new Description("Egg, cheese, and milk"),
-                        new StartDateTime("10/01/2016 1900"), new EndDateTime("10/01/2016 2100"),
-                        new UniqueTagList("Incomplete")),
-                new Task(new Name("Finish report"), new Description("Do up citations and references"),
-                        new StartDateTime("15/03/2016 1000"), new EndDateTime("23/03/2016 2359"),
-                        new UniqueTagList("Incomplete")),
-                new Task(new Name("Take out the trash"), new Description(""),
-                        new StartDateTime("25/12/2015 1400"), new EndDateTime("25/12/2015 1415"),
-                        new UniqueTagList("Complete")),
-                new Task(new Name("Do Christmas shopping"), new Description("Compare prices with Amazon"),
-                        new StartDateTime("20/12/2015 1200"), new EndDateTime("24/12/2015 2200"),
-                        new UniqueTagList("Complete")),
-                new Task(new Name("Learn how to cook"), new Description("Google instant noodle recipes"),
-                        new StartDateTime("11/11/2015 0900"), new EndDateTime("11/11/2015 1900"),
-                        new UniqueTagList("Complete")) };
+                };
         } catch (IllegalValueException e) {
             throw new AssertionError("sample data cannot be invalid", e);
         }
