1179fbd2348f3976d74e5a75bc824f77f7469d50
pzyu <pangzhengyu@live.com>
2017-04-06 21:46:38 +0800

    Update task card layout Add start date time/end date time to title if any Add strikethrough to completed task

--- src/main/java/werkbook/task/ui/TaskCard.java
+++ src/main/java/werkbook/task/ui/TaskCard.java
@@ -1,12 +1,13 @@
 package werkbook.task.ui;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Accordion;
 import javafx.scene.control.Label;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import werkbook.task.model.task.ReadOnlyTask;
 
 public class TaskCard extends UiPart<Region> {
@@ -16,7 +17,7 @@ public class TaskCard extends UiPart<Region> {
     @FXML
     private HBox cardPane;
     @FXML
-    private Label name;
+    private Text name;
     @FXML
     private Label id;
     @FXML
@@ -24,27 +25,49 @@ public class TaskCard extends UiPart<Region> {
     @FXML
     private Label startDateTime;
     @FXML
+    private Label headerStartDateTime;
+    @FXML
     private Label endDateTime;
+    @FXML
+    private Label headerEndDateTime;
     @FXML
     private FlowPane tags;
     @FXML
     private TitledPane titledPane;
     @FXML
-    private Accordion accordion;
-
+    private VBox header;
 
     public TaskCard(ReadOnlyTask task, int displayedIndex, int selectionIndex) {
         super(FXML);
         name.setText(task.getName().taskName);
         id.setText(displayedIndex + ". ");
         description.setText(task.getDescription().toString());
-        startDateTime.setText(task.getStartDateTime().getPrettyString());
-        endDateTime.setText(task.getEndDateTime().getPrettyString());
+        startDateTime.setText(task.getStartDateTime().toString());
+        endDateTime.setText(task.getEndDateTime().toString());
+        
+        headerStartDateTime.setText(task.getStartDateTime().getPrettyString());
+        headerEndDateTime.setText(task.getEndDateTime().getPrettyString());
         initTags(task);
-
-        if (selectionIndex == displayedIndex - 1) {
-            accordion.setExpandedPane(titledPane);
+        
+        // If start date time is not present, then remove
+        if (!task.getStartDateTime().isPresent()) {
+            header.getChildren().remove(1);
+            // If end date time is not present, then remove
+            if (!task.getEndDateTime().isPresent()) { 
+                header.getChildren().remove(1);
+            }
         }
+        
+        // Set strikethrough if task is complete
+        if (task.getTags().asObservableList().get(0).tagName.equals("Complete")) {
+            name.setStrikethrough(true);
+        }
+        
+        titledPane.setExpanded(false);
+        if (selectionIndex == displayedIndex - 1) {
+            titledPane.setExpanded(true);
+        }
+        
     }
 
     private void initTags(ReadOnlyTask task) {
