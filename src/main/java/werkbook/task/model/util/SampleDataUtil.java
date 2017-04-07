f750f5a454d40403341748dffbc68c6b56660977
pzyu <pangzhengyu@live.com>
2017-04-07 18:00:56 +0800

    Fix checkstyle errors

--- src/main/java/werkbook/task/model/util/SampleDataUtil.java
+++ src/main/java/werkbook/task/model/util/SampleDataUtil.java
@@ -13,22 +13,18 @@ import werkbook.task.model.task.Name;
 import werkbook.task.model.task.StartDateTime;
 import werkbook.task.model.task.Task;
 import werkbook.task.model.task.UniqueTaskList;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 public class SampleDataUtil {
     public static Task[] getSampleTasks() {
-    	// @@author A0130183U
-    	Calendar cal = Calendar.getInstance();
-    	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HHmm");
+        // @@author A0130183U
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HHmm");
         try {
-            return new Task[] {
-                new Task(new Name("Welcome to Werkbook! Start by adding a task."),
-                        new Description("Try: add <name of task> (description of task)"),
-                        new StartDateTime("03/01/2016 1100"), new EndDateTime(sdf.format(cal.getTime())),
-                        new UniqueTagList("Incomplete")),
-            	};
-         // @@author
+            return new Task[] { new Task(new Name("Welcome to Werkbook! Start by adding a task."),
+                    new Description("Try: add <name of task> (description of task)"),
+                    new StartDateTime("03/01/2016 1100"), new EndDateTime(sdf.format(cal.getTime())),
+                    new UniqueTagList("Incomplete")), };
+            // @@author
         } catch (IllegalValueException e) {
             throw new AssertionError("sample data cannot be invalid", e);
         }
