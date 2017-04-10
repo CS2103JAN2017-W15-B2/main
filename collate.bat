a50918561988e3c2753f5530e91c5f329824b67a
FausticSun <fausticsun@gmail.com>
2017-04-10 14:06:41 +0800

    Set proper encoding for collation

--- collate.bat
+++ collate.bat
@@ -1,5 +1,5 @@
-java -jar Collate-TUI.jar collate from src/main to collated/main include java, fxml, css
+java -Dfile.encoding=UTF8 -jar Collate-TUI.jar collate from src/main to collated/main include java, fxml, css
 
-java -jar Collate-TUI.jar collate from src/test to collated/test include java
+java -Dfile.encoding=UTF8 -jar Collate-TUI.jar collate from src/test to collated/test include java
 
-java -jar Collate-TUI.jar collate from unused to collated/unused include java, fxml, css
+java -Dfile.encoding=UTF8 -jar Collate-TUI.jar collate from unused to collated/unused include java, fxml, css
